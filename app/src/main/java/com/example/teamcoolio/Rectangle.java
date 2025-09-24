88dafb62bf2bc44a88fc1a0953d7f6640ace61ac
ParthMittal2017 <pmittal1@ualberta.ca>
2025-09-24 17:50:49 -0600

    Added Rectangle shape

---
+++ app/src/main/java/com/example/teamcoolio/Rectangle.java
@@ -0,0 +1,11 @@
+package com.example.teamcoolio;
+public class Rectangle extends Shape {
+    private int width;
+    private int height;
+    public Rectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
